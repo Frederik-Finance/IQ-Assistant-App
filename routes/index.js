500b901e61926ad57116b63003a62a8aab137bb0
Fredrik123-crypto <fwoite@gmail.com>
2023-10-04 14:17:26 +0200

    Paypal Integration

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,10 @@ const vision = require('@google-cloud/vision');
 const User = require('../models/user.js');
 const { OpenAI } = require('openai');
 const mongoose = require('mongoose');
-require('dotenv').config();
+let fetch;
+import('node-fetch').then(module => {
+    fetch = module.default;
+}); require('dotenv').config();
 
 // Configure multer for memory storage
 const storage = multer.memoryStorage();
@@ -23,12 +26,36 @@ const API_KEY_VISION = process.env.API_KEY_VISION;
 const API_KEY_GPT4 = process.env.API_KEY_GPT4;
 const VISION_API_URL = `https://vision.googleapis.com/v1/images:annotate?key=${API_KEY_VISION}`;
 
+
+
+// PAYPAL
+const PAYPAL_CLIENT_ID = process.env.PAYPAL_CLIENT_ID;
+const PAYPAL_CLIENT_SECRET = process.env.PAYPAL_CLIENT_SECRET;
+const base = "https://api-m.sandbox.paypal.com";
+
 // Configure OpenAI API
 const openai = new OpenAI({
     apiKey: API_KEY_GPT4
 })
 
 
+const incrementUserQuestions = async (userEmail, amount = 1) => {
+    try {
+        const updatedUser = await User.findOneAndUpdate(
+            { userEmail: userEmail },
+            { $inc: { availableQuestions: amount } },
+            { new: true, runValidators: true }  // 'new' option returns the updated document
+        );
+
+        if (!updatedUser) {
+            throw new Error('User not found.');
+        }
+
+        return updatedUser;
+    } catch (err) {
+        throw err;
+    }
+};
 
 
 /**
@@ -256,6 +283,172 @@ router.post('/submit-images', upload.array('screenshots'), requiresAuth(), async
         res.status(500).json({ success: false, message: "Internal server error" });
     }
 });
+
+
+// PAYPAL PAYMENTS
+const generateAccessToken = async () => {
+    try {
+        if (!PAYPAL_CLIENT_ID || !PAYPAL_CLIENT_SECRET) {
+            throw new Error("MISSING_API_CREDENTIALS");
+        }
+        const auth = Buffer.from(
+            PAYPAL_CLIENT_ID + ":" + PAYPAL_CLIENT_SECRET,
+        ).toString("base64");
+        const response = await fetch(`${base}/v1/oauth2/token`, {
+            method: "POST",
+            body: "grant_type=client_credentials",
+            headers: {
+                Authorization: `Basic ${auth}`,
+            },
+        });
+
+        const data = await response.json();
+        return data.access_token;
+    } catch (error) {
+        console.error("Failed to generate Access Token:", error);
+    }
+};
+
+/**
+* Create an order to start the transaction.
+* @see https://developer.paypal.com/docs/api/orders/v2/#orders_create
+*/
+
+const createOrder = async (cartDetails) => {
+    console.log(
+        "shopping cart information passed from the frontend createOrder() callback:",
+        cartDetails,
+    );
+
+    const accessToken = await generateAccessToken();
+    const url = `${base}/v2/checkout/orders`;
+    const payload = {
+        intent: "CAPTURE",
+        purchase_units: [{
+            amount: cartDetails.amount
+        }]
+    };
+
+    const response = await fetch(url, {
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${accessToken}`,
+            // Uncomment one of these to force an error for negative testing (in sandbox mode only). Documentation:
+            // https://developer.paypal.com/tools/sandbox/negative-testing/request-headers/
+            // "PayPal-Mock-Response": '{"mock_application_codes": "MISSING_REQUIRED_PARAMETER"}'
+            // "PayPal-Mock-Response": '{"mock_application_codes": "PERMISSION_DENIED"}'
+            // "PayPal-Mock-Response": '{"mock_application_codes": "INTERNAL_SERVER_ERROR"}'
+        },
+        method: "POST",
+        body: JSON.stringify(payload),
+    });
+
+    return handleResponse(response);
+};
+
+
+async function handleResponse(response) {
+    try {
+        const jsonResponse = await response.json();
+        return {
+            jsonResponse,
+            httpStatusCode: response.status,
+        };
+    } catch (err) {
+        const errorMessage = await response.text();
+        throw new Error(errorMessage);
+    }
+}
+
+
+const captureOrder = async (orderID, userEmail, questionCount) => {
+    const accessToken = await generateAccessToken();
+    const url = `${base}/v2/checkout/orders/${orderID}/capture`;
+
+    const response = await fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${accessToken}`,
+            // Uncomment one of these to force an error for negative testing (in sandbox mode only). Documentation:
+            // https://developer.paypal.com/tools/sandbox/negative-testing/request-headers/
+            // "PayPal-Mock-Response": '{"mock_application_codes": "INSTRUMENT_DECLINED"}'
+            // "PayPal-Mock-Response": '{"mock_application_codes": "TRANSACTION_REFUSED"}'
+            // "PayPal-Mock-Response": '{"mock_application_codes": "INTERNAL_SERVER_ERROR"}'
+        },
+    });
+
+    const result = await handleResponse(response);
+
+    if (result.httpStatusCode === 201 && userEmail && questionCount) {
+        // If the order capture was successful, increment the user's question count
+        await incrementUserQuestions(userEmail, questionCount);
+    }
+
+    return result;
+};
+
+
+router.post("/api/orders", async (req, res) => {
+    try {
+        // Extract the quantity of questions from the cart passed from the frontend
+        const quantity = parseInt(req.body.cart[0].quantity, 10);
+
+        // Calculate the total price
+        const totalPrice = calculatePrice(quantity);
+
+        const cartDetails = {
+            id: "Questions",
+            amount: {
+                currency_code: "EUR",
+                value: totalPrice.toString()
+            }
+        };
+
+        const { jsonResponse, httpStatusCode } = await createOrder(cartDetails);
+        res.status(httpStatusCode).json(jsonResponse);
+    } catch (error) {
+        console.error("Failed to create order:", error);
+        res.status(500).json({ error: "Failed to create order." });
+    }
+});
+
+function calculatePrice(quantity) {
+    let price = 1; // default price for 1 question
+
+    if (quantity >= 10 && quantity <= 19) {
+        price = 1 - 0.05; // 5% discount
+    } else if (quantity >= 20 && quantity <= 29) {
+        price = 1 - 0.10; // 10% discount
+    } else if (quantity >= 30 && quantity <= 39) {
+        price = 1 - 0.15; // 15% discount
+    } else if (quantity >= 40 && quantity <= 49) {
+        price = 1 - 0.17; // 17% discount
+    } else if (quantity >= 50) {
+        price = 1 - 0.20; // 20% discount
+    }
+
+    return price * quantity;
+}
+
+
+router.post("/api/orders/:orderID/capture", requiresAuth(), async (req, res) => {
+    try {
+        const { orderID } = req.params;
+        const userEmail = req.oidc.user.email;
+        // Assuming the frontend sends the number of questions the user has purchased
+        const questionCount = req.body.quantity;
+        console.log(questionCount)
+
+
+        const { jsonResponse, httpStatusCode } = await captureOrder(orderID, userEmail, questionCount);
+        res.status(httpStatusCode).json(jsonResponse);
+    } catch (error) {
+        console.error("Failed to capture order:", error);
+        res.status(500).json({ error: "Failed to capture order." });
+    }
+});
+
 
 
 
